fef2c09beb038cc19bf2ffee6baa1c39464aec25
Alex Jones <alexander.jones@softwire.com>
2024-01-26 15:34:08 +0000

    Add support for inrush current and long lead calculations

--- scripts/main.js
+++ scripts/main.js
@@ -1,79 +1,87 @@
+import { setupDynamicFormBehaviour } from "./dynamic_content/dynamic_form_content.js";
 import { fuseRatingTable } from "./fuse_rating_table.js"
 
-const POWER_RATING_MARGIN_OF_ERROR = 0.1 // 10% margin of error for power ratings
-const AC_LOWER_SUPPLY_VOLTAGE = 230 * 0.94 // 230V minus 6% - needed to estimate maximum current draw from appliance
-
 function calculatorFormSubmit(event) {
-    // Implicitly raises a "formdata" event (so we can actually read the data and use it!)
-    new FormData(event.target);
+  // Implicitly raises a "formdata" event (so we can actually read the data and use it!)
+  new FormData(event.target);
 
-    // Suppresses the default form behaviour of directing to a new page on page submit
-    event.preventDefault()
+  // Suppresses the default form behaviour of directing to a new page on page submit
+  event.preventDefault()
 }
 
 function calculatorProcessData(event) {
-    const data = event.formData;
+  const data = event.formData;
 
-    const power = data.get("power")
-    const csa = data.get("csa")
-    const fuse = parseInt(data.get("fuse"))
-    const plug = data.get("plug")
+  const power = data.get("power")
+  const voltage = parseInt(data.get("voltage"))
+  const inrush = data.get("inrush")
+  const short_lead = data.get("short-lead")
+  const csa = data.get("csa")
+  const fuse = parseInt(data.get("fuse"))
+  const plug = data.get("plug")
 
 
-    const powerUpperBound = power * (1.0 + POWER_RATING_MARGIN_OF_ERROR)
+  const currentDraw = power / voltage
+  const roundedCurrentDrawString = currentDraw.toLocaleString(
+    "en-GB",
+    {
+      maximumFractionDigits: 2,
+      minimumFractionDigits: 2
+    }
+  );
 
-    const maximumPotentialCurrentDraw = powerUpperBound / AC_LOWER_SUPPLY_VOLTAGE
-    const roundedMaxCurrentDrawString = maximumPotentialCurrentDraw.toLocaleString(
-        "en-GB", 
-        { 
-            maximumFractionDigits: 2, 
-            minimumFractionDigits: 2 
-        }
-    );
+  let warningMessage = ""
 
-    let warningMessage = ""
-
-    if (fuse < maximumPotentialCurrentDraw) {
-        warningMessage += 
-        `Fuse rating too low for device! 
-        Should be at least ${ roundedMaxCurrentDrawString }
+  if (fuse < currentDraw) {
+    warningMessage +=
+      `Fuse rating too low for device! 
+        Should be at least ${roundedCurrentDrawString}
         `
-    }
+  }
 
-    const fuseRatingOfCable = fuseRatingTable[csa][plug]
+  let fuseRatingOfCable = fuseRatingTable[csa]["standard"][plug]
 
-    if (fuse > fuseRatingOfCable) {
-        warningMessage += 
-        `Fuse rating too high for the combination of flex/cable! 
-        Should at most be ${ fuseRatingOfCable }
+  if (csa === "0.5" && inrush) {
+    fuseRatingOfCable = fuseRatingTable[csa]["inrush"][plug]
+  }
+
+  if (csa === "0.75" && short_lead) {
+    fuseRatingOfCable = fuseRatingTable[csa]["short_lead"][plug]
+  }
+
+  if (fuse > fuseRatingOfCable) {
+    warningMessage +=
+      `Fuse rating too high for the combination of flex/cable! 
+        Should at most be ${fuseRatingOfCable}
         `
-    }
-    
-    if (warningMessage !== "") {
-        generateDisplayMessage(warningMessage)
-        return
-    }
+  }
 
-    const evaluationMessage = 
+  if (warningMessage !== "") {
+    generateDisplayMessage(warningMessage)
+    return
+  }
+
+  const evaluationMessage =
     `No Concerns Flagged for this Combination 
-    Max Amps of Device: ${ roundedMaxCurrentDrawString } 
-    Fuse Rating of Cable & Plug: ${ fuseRatingOfCable }
+    Max Amps of Device: ${roundedCurrentDrawString} 
+    Fuse Rating of Cable & Plug: ${fuseRatingOfCable}
     `
 
-    
-    generateDisplayMessage(evaluationMessage)
+
+  generateDisplayMessage(evaluationMessage)
 }
 
 function generateDisplayMessage(message) {
-    const outputElement = document.getElementById('outcome')
+  const outputElement = document.getElementById('outcome')
 
-    outputElement.innerText = message
+  outputElement.innerText = message
 }
 
 function main() {
-    const calculatorForm = document.getElementById('calculator-form')
-    calculatorForm.addEventListener('submit', calculatorFormSubmit)
-    calculatorForm.addEventListener('formdata', calculatorProcessData)
+  const calculatorForm = document.getElementById('calculator-form')
+  calculatorForm.addEventListener('submit', calculatorFormSubmit)
+  calculatorForm.addEventListener('formdata', calculatorProcessData)
+  setupDynamicFormBehaviour()
 }
 
 main()
