f6492404ef0f66b7088028eb22acd835266b5675
Alex Jones <alexander.jones@softwire.com>
2024-01-26 15:42:09 +0000

    Revert indentation to ease reviewing

--- scripts/main.js
+++ scripts/main.js
@@ -2,86 +2,86 @@ import { setupDynamicFormBehaviour } from "./dynamic_content/dynamic_form_conten
 import { fuseRatingTable } from "./fuse_rating_table.js"
 
 function calculatorFormSubmit(event) {
-  // Implicitly raises a "formdata" event (so we can actually read the data and use it!)
-  new FormData(event.target);
+    // Implicitly raises a "formdata" event (so we can actually read the data and use it!)
+    new FormData(event.target);
 
-  // Suppresses the default form behaviour of directing to a new page on page submit
-  event.preventDefault()
+    // Suppresses the default form behaviour of directing to a new page on page submit
+    event.preventDefault()
 }
 
 function calculatorProcessData(event) {
-  const data = event.formData;
+    const data = event.formData;
 
-  const power = data.get("power")
-  const voltage = parseInt(data.get("voltage"))
-  const inrush = data.get("inrush")
-  const short_lead = data.get("short-lead")
-  const csa = data.get("csa")
-  const fuse = parseInt(data.get("fuse"))
-  const plug = data.get("plug")
+    const power = data.get("power")
+    const voltage = parseInt(data.get("voltage"))
+    const inrush = data.get("inrush")
+    const short_lead = data.get("short-lead")
+    const csa = data.get("csa")
+    const fuse = parseInt(data.get("fuse"))
+    const plug = data.get("plug")
 
 
-  const currentDraw = power / voltage
-  const roundedCurrentDrawString = currentDraw.toLocaleString(
-    "en-GB",
-    {
-      maximumFractionDigits: 2,
-      minimumFractionDigits: 2
-    }
-  );
+    const currentDraw = power / voltage
+    const roundedCurrentDrawString = currentDraw.toLocaleString(
+        "en-GB",
+        {
+            maximumFractionDigits: 2,
+            minimumFractionDigits: 2
+        }
+    );
 
-  let warningMessage = ""
+    let warningMessage = ""
 
-  if (fuse < currentDraw) {
-    warningMessage +=
-      `Fuse rating too low for device! 
+    if (fuse < currentDraw) {
+        warningMessage +=
+            `Fuse rating too low for device! 
         Should be at least ${roundedCurrentDrawString}
         `
-  }
+    }
 
-  let fuseRatingOfCable = fuseRatingTable[csa]["standard"][plug]
+    let fuseRatingOfCable = fuseRatingTable[csa]["standard"][plug]
 
-  if (csa === "0.5" && inrush) {
-    fuseRatingOfCable = fuseRatingTable[csa]["inrush"][plug]
-  }
+    if (csa === "0.5" && inrush) {
+        fuseRatingOfCable = fuseRatingTable[csa]["inrush"][plug]
+    }
 
-  if (csa === "0.75" && short_lead) {
-    fuseRatingOfCable = fuseRatingTable[csa]["short_lead"][plug]
-  }
+    if (csa === "0.75" && short_lead) {
+        fuseRatingOfCable = fuseRatingTable[csa]["short_lead"][plug]
+    }
 
-  if (fuse > fuseRatingOfCable) {
-    warningMessage +=
-      `Fuse rating too high for the combination of flex/cable! 
+    if (fuse > fuseRatingOfCable) {
+        warningMessage +=
+            `Fuse rating too high for the combination of flex/cable! 
         Should at most be ${fuseRatingOfCable}
         `
-  }
+    }
 
-  if (warningMessage !== "") {
-    generateDisplayMessage(warningMessage)
-    return
-  }
+    if (warningMessage !== "") {
+        generateDisplayMessage(warningMessage)
+        return
+    }
 
-  const evaluationMessage =
-    `No Concerns Flagged for this Combination 
+    const evaluationMessage =
+        `No Concerns Flagged for this Combination 
     Max Amps of Device: ${roundedCurrentDrawString} 
     Fuse Rating of Cable & Plug: ${fuseRatingOfCable}
     `
 
 
-  generateDisplayMessage(evaluationMessage)
+    generateDisplayMessage(evaluationMessage)
 }
 
 function generateDisplayMessage(message) {
-  const outputElement = document.getElementById('outcome')
+    const outputElement = document.getElementById('outcome')
 
-  outputElement.innerText = message
+    outputElement.innerText = message
 }
 
 function main() {
-  const calculatorForm = document.getElementById('calculator-form')
-  calculatorForm.addEventListener('submit', calculatorFormSubmit)
-  calculatorForm.addEventListener('formdata', calculatorProcessData)
-  setupDynamicFormBehaviour()
+    const calculatorForm = document.getElementById('calculator-form')
+    calculatorForm.addEventListener('submit', calculatorFormSubmit)
+    calculatorForm.addEventListener('formdata', calculatorProcessData)
+    setupDynamicFormBehaviour()
 }
 
 main()
