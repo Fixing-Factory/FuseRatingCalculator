ab42e73e4fc93d6a6ce01ea74172791bd707ae78
Alex Jones <alexander.jones@softwire.com>
2024-01-27 16:23:09 +0000

    Adjust whitespace

--- scripts/dynamic_content/dynamic_form_content.js
+++ scripts/dynamic_content/dynamic_form_content.js
@@ -1,27 +1,27 @@
 export function setupDynamicFormBehaviour() {
-    const csaInput = document.getElementById('csa')
-    const inrushCurrentFormBox = document.getElementById('inrush-selection')
-    const shortLeadFormBox = document.getElementById('short-lead-selection')
+  const csaInput = document.getElementById('csa')
+  const inrushCurrentFormBox = document.getElementById('inrush-selection')
+  const shortLeadFormBox = document.getElementById('short-lead-selection')
 
-    function onCSAChange() {
-        switch (csaInput.value) {
-            case "0.5": {
-                inrushCurrentFormBox.classList.remove('disabled')
-                shortLeadFormBox.classList.add('disabled')
-                break
-            }
-            case "0.75": {
-                inrushCurrentFormBox.classList.add('disabled')
-                shortLeadFormBox.classList.remove('disabled')
-                break
-            }
-            default: {
-                inrushCurrentFormBox.classList.add('disabled')
-                shortLeadFormBox.classList.add('disabled')
-                break
-            }
-        }
+  function onCSAChange() {
+    switch (csaInput.value) {
+      case "0.5": {
+        inrushCurrentFormBox.classList.remove('disabled')
+        shortLeadFormBox.classList.add('disabled')
+        break
+      }
+      case "0.75": {
+        inrushCurrentFormBox.classList.add('disabled')
+        shortLeadFormBox.classList.remove('disabled')
+        break
+      }
+      default: {
+        inrushCurrentFormBox.classList.add('disabled')
+        shortLeadFormBox.classList.add('disabled')
+        break
+      }
     }
+  }
 
-    csaInput.addEventListener("change", onCSAChange)
+  csaInput.addEventListener("change", onCSAChange)
 }
