ff3845f63f93cd79bd8d6f0d2980990c7a711f2e
Alex Jones <alexander.jones@softwire.com>
2024-03-31 22:04:11 +0100

    Update guidance

--- scripts/dynamic_content/advisory_info_dialog.js
+++ scripts/dynamic_content/advisory_info_dialog.js
@@ -1,18 +1,62 @@
+const MIN_PAGE_NUMBER = 1
+const MAX_PAGE_NUMBER = 2
+
 export class AdvisoryInfoDialogManager {
   constructor() {
     this.infoIcon = document.getElementById('info-icon')
+    this.closeButton = document.getElementById('close-dialog')
+    this.pageForward = document.getElementById('page-forward')
+    this.pageBack = document.getElementById('page-back')
     this.advisoryDialog = document.getElementById('advisory-dialog')
+    this.currentPage = 1
   }
 
   initialiseDialog() {
     this.infoIcon.addEventListener("click", () => { this.onInfoButtonClick() })
+    this.closeButton.addEventListener("click", () => { this.onCloseButtonClick() })
+    this.pageForward.addEventListener("click", () => { this.onPageForwardClick() })
+    this.pageBack.addEventListener("click", () => { this.onPageBackClick() })
   }
 
   onInfoButtonClick() {
-    if (this.advisoryDialog.open) {
-      this.advisoryDialog.close()
-    } else{
-      this.advisoryDialog.show()
+    this.advisoryDialog.showModal()
+  }
+
+  onCloseButtonClick() {
+    this.advisoryDialog.close()
+  }
+
+  onPageForwardClick() {
+    let currentParagraph = document.getElementById(`dialog-page-${this.currentPage}`)
+    currentParagraph.classList.add("disabled")
+
+    if (this.currentPage === MIN_PAGE_NUMBER) {
+      this.pageBack.classList.remove("disabled")
+    }
+
+    this.currentPage += 1
+    currentParagraph = document.getElementById(`dialog-page-${this.currentPage}`)
+    currentParagraph.classList.remove("disabled")
+
+    if (this.currentPage === MAX_PAGE_NUMBER) {
+      this.pageForward.classList.add("disabled")
+    }
+  }
+
+  onPageBackClick() {
+    let currentParagraph = document.getElementById(`dialog-page-${this.currentPage}`)
+    currentParagraph.classList.add("disabled")
+
+    if (this.currentPage === MAX_PAGE_NUMBER) {
+      this.pageForward.classList.remove("disabled")
+    }
+
+    this.currentPage -= 1
+    currentParagraph = document.getElementById(`dialog-page-${this.currentPage}`)
+    currentParagraph.classList.remove("disabled")
+
+    if (this.currentPage === MIN_PAGE_NUMBER) {
+      this.pageBack.classList.add("disabled")
     }
   }
 }
